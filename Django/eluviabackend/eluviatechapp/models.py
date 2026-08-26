77cb6ff9ae79f2221c32cc2765a37b0696164db9
Mika <perezlopezmicael@gmail.com>
2026-08-26 19:04:52 -0400

    agregando modelos

--- Django/eluviabackend/eluviatechapp/models.py
+++ Django/eluviabackend/eluviatechapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# models.py
+from django.db import models
+
+
+class Tickets(models.Model):
+    categoria = models.CharField(max_length=50)
+    severidad = models.CharField(max_length=20)
+    asunto = models.CharField(max_length=150)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=40)
+    fecha = models.DateTimeField(auto_now_add=True)
+    fecha_cierre = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.asunto
