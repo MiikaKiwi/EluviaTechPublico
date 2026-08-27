937fbb9eab27bc5b9d94e062bf18ce33c3e4a95c
Mika <perezlopezmicael@gmail.com>
2026-08-27 15:08:46 -0400

    Agregado los modelos al sql v1

--- Django/eluviabackend/eluviabackend/settings.py
+++ Django/eluviabackend/eluviabackend/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'eluviabackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backend',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3307',
     }
 }
 
